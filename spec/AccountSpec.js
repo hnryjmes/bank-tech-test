e99c8772f2a0ebb36d96cbbe32d239805487b953
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-03 12:42:54 +0000

    Add first test to create an account

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,2 +1,8 @@
-describe("Account", () => {
+describe('Account', () => {
+  let Account = require('../lib/Account');
+
+  it('can create an account', () => {
+    let account = new Account();
+    expect(account instanceof Account).toBe(true);
+  });
 });
