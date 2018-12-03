cb8bcc4937fbb10f86f49e13d5bc680396aec760
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-03 16:40:54 +0000

    Remove feature that user can enter date when creating new transaction

--- spec/TransactionSpec.js
+++ spec/TransactionSpec.js
@@ -3,15 +3,8 @@ describe('Transaction', () => {
 
   it('stores a transaction with today\'s date by default', () => {
     const today = new Date();
-    const transaction = new Transaction('deposit', 1, today);
+    const transaction = new Transaction('deposit', 1);
 
     expect(transaction.getReadableDate()).toEqual(today.toLocaleDateString());
-  });
-
-  it('stores a transaction with a custom date', () => {
-    const myDate = new Date('2018-12-02');
-    const transaction = new Transaction('deposit', 1, myDate);
-
-    expect(transaction.getReadableDate()).toEqual(myDate.toLocaleDateString());
   });
 });
