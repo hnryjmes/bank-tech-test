5b56bf2339d9506d81af70db56d659fcc11a82a8
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-03 14:03:47 +0000

    Refactor AccountSpec.js to use beforeEach

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -2,14 +2,17 @@
 /* eslint-disable no-undef */
 describe('Account', () => {
   const Account = require('../lib/Account');
+  let account;
+
+  beforeEach(() => {
+    account = new Account();
+  });
 
   it('has zero balance by default', () => {
-    const account = new Account();
     expect(account.balance).toEqual(0);
   });
 
   it('can make a deposit of 1', () => {
-    const account = new Account();
     account.deposit(1);
     expect(account.balance).toEqual(1);
   });
