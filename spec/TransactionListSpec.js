f7bae134c663d8f92103f038680c791f5652fec8
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-03 16:14:46 +0000

    Add test for statement with one transaction

--- spec/TransactionListSpec.js
+++ spec/TransactionListSpec.js
@@ -14,6 +14,18 @@ describe('TransactionList', () => {
   });
 
   it('returns an empty statement when there are no transactions', () => {
-    expect(transactionList.getStatement()).toEqual(transactionList.HEADINGS.join(transactionList.JOINER));
+    let statementHeader = transactionList.HEADINGS.join(transactionList.JOINER);
+    
+    expect(transactionList.getStatement()).toEqual(statementHeader);
+  });
+
+  it('returns a statement with one transaction', () => {
+    transactionList.append(new Transaction('deposit', 1, new Date()));
+    let statementHeader = transactionList.HEADINGS.join(transactionList.JOINER);
+    let transaction = transactionList.getTransactions()[0];
+    let firstStatementItem = [transaction.getReadableDate(), 1].join(transactionList.JOINER);
+    let statement = [statementHeader, firstStatementItem].join('\n');
+
+    expect(transactionList.getStatement()).toEqual(statement);
   });
 });
