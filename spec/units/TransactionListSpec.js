11353cd08e9ed95669847238541e7a380898e154
Henry Cooksley <henry.cooksley@gmail.com>
2018-12-04 09:36:49 +0000

    Isolate unit test for TransactionList using Jasmine spies

--- spec/units/TransactionListSpec.js
+++ spec/units/TransactionListSpec.js
@@ -1,14 +1,18 @@
 describe('TransactionList', () => {
   const TransactionList = require('../../lib/TransactionList');
-  const Transaction = require('../../lib/Transaction');
   let transactionList;
+  let transaction;
 
   beforeEach(() => {
     transactionList = new TransactionList();
+    transaction = jasmine.createSpyObj('transaction', {
+      getReadableDate: '25/12/2018',
+      getReadableAmount: '1.00'
+    });
   });
 
   it('stores a list of transactions', () => {
-    transactionList.append(new Transaction('deposit', 1, new Date()));
+    transactionList.append(transaction);
 
     expect(transactionList.getTransactions().length).toEqual(1);
   });
@@ -20,9 +24,9 @@ describe('TransactionList', () => {
   });
 
   it('returns a statement with one transaction', () => {
-    transactionList.append(new Transaction('deposit', 1));
+    transactionList.append(transaction);
     let statementHeader = transactionList.HEADINGS.join(transactionList.JOINER);
-    let transaction = transactionList.getTransactions()[0];
+    transaction = transactionList.getTransactions()[0];
     let firstStatementItem = [transaction.getReadableDate(), transaction.getReadableAmount()].join(transactionList.JOINER);
     let statement = [statementHeader, firstStatementItem].join('\n');
 
@@ -30,16 +34,22 @@ describe('TransactionList', () => {
   });
   //TODO: Make statement same as acceptance criteria!
   it('returns a statement with multiple historical transations', () => {
-    let firstTransaction = new Transaction('deposit', 1000);
-    firstTransaction.date = new Date('2012-01-10');
+    let firstTransaction = jasmine.createSpyObj('firstTransaction', {
+      getReadableDate: '10/01/2012',
+      getReadableAmount: '1000.00'
+    });
     transactionList.append(firstTransaction);
 
-    let secondTransaction = new Transaction('deposit', 2000);
-    secondTransaction.date = new Date('2012-01-13');
+    let secondTransaction = jasmine.createSpyObj('secondTransaction', {
+      getReadableDate: '13/01/2012',
+      getReadableAmount: '2000.00'
+    });
     transactionList.append(secondTransaction);
 
-    let thirdTransaction = new Transaction('withdraw', 500);
-    thirdTransaction.date = new Date('2012-01-14');
+    let thirdTransaction = jasmine.createSpyObj('thirdTransaction', {
+      getReadableDate: '14/01/2012',
+      getReadableAmount: '500.00'
+    });
     transactionList.append(thirdTransaction);
 
     let statementHeader = transactionList.HEADINGS.join(transactionList.JOINER);
